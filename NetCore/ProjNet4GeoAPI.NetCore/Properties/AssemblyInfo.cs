45e1446b31c4f32593d845b2169a10cc5187bf9b
Brandon Perry <Bperry@osisoft.com>
2016-08-27 11:12:24 -0500

    Added NetCore port of PCL solution

---
+++ NetCore/ProjNet4GeoAPI.NetCore/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ProjNet4GeoAPI.NetCore")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cedfd95e-664a-426a-b6d4-4a18f3bdbaba")]
---
+++ NetCore/ProjNet4GeoAPI.NetCore/Utility/TypeExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace ProjNet.Utility
+{
+    public static class TypeExtensions
+    {
+#if NETSTANDARD1_6
+        public static bool IsAssignableFrom(this Type t, Type other)
+        {
+            return t.GetTypeInfo().IsAssignableFrom(other);
+        }
+
+        public static ConstructorInfo GetConstructor(this Type t, Type[] types)
+        {
+            return t.GetTypeInfo().GetConstructor(types);
+        }
+#endif
+    }
+}
